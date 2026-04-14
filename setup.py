ab6311d73763974ff445d468c0182ddc8edfd521
Valentina <valentina.pericu@gmail.com>
2026-04-14 08:51:41 +0000

    [add] add and handle .yaml config file for each region

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         (os.path.join('share', package_name), ['package.xml', *glob.glob('launch/*')]),
+        (os.path.join('share', package_name, 'config'), glob.glob('config/*.yaml')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
